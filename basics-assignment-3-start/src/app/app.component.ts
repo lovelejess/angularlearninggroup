45cf6d2facc01e9fbc94446a650809a55f64fcd0
riojack <ayjay122@gmail.com>
2018-01-16 08:19:43 -0600

    Mobbed completion of assignment 3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public isDisplayed = true;
+  private buttonClicks: string[] = [];
+  private counter: number = 0;
+
+  public toggleDisplay() {
+    this.isDisplayed = !this.isDisplayed;
+    this.counter++;
+    this.buttonClicks.push(`button clicked ${this.counter} times`);
+  }
 }
