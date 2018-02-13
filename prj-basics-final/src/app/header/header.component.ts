a1cd6fa22d1f8736c5b9a64a1aa3a2a1a1708f99
Jessica Le <jessicaduongle@gmail.com>
2018-02-13 07:48:49 -0600

    Adding an ngIf for shoppingList

--- prj-basics-final/src/app/header/header.component.ts
+++ prj-basics-final/src/app/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() shoplistIsEnabled: EventEmitter<boolean> = new EventEmitter();
 
+    onRecipesNavigation() {
+      this.shoplistIsEnabled.emit(false);
+    }
+
+    onShoppingListNavigation() {
+      this.shoplistIsEnabled.emit(true);
+    }
 }
